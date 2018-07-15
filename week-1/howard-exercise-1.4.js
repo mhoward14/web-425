64d5bd4c345d946f8217a35589645cc8ce130bce
Matthew Howard <mhoward14@icloud.com>
2018-07-15 16:17:26 -0600

    TypeScript and ES5 Exercise

---
+++ week-1/howard-exercise-1.4.js
@@ -0,0 +1,6 @@
+var firstName = 'Matthew';
+var lastName = 'Howard';
+function myName(firstName, lastName) {
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+console.log(myName(firstName, lastName));
--- week-1/howard-exercise-1.4.ts
+++ week-1/howard-exercise-1.4.ts
@@ -4,4 +4,5 @@ let lastName: string = 'Howard';
 function myName(firstName: string, lastName: string): string {
     return 'Hello ' + firstName + ' ' + lastName;
 }
-console.log(firstName, lastName);
+
+console.log(myName(firstName, lastName));
