90f55773b1c72789ea3c7cf29834aa7cf92b8e68
Matthew Howard <mhoward14@icloud.com>
2018-07-15 16:52:27 -0600

    Angular Modules Exercise

---
+++ week-1/hello-modules/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Welcome to the AppComponent</h1><app-shipping></app-shipping>',
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ week-1/hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p>
+      Shipping Works! It gets packages from one place to another!
+    </p>
+  `,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
