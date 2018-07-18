a9b14957ca310819039eb46e826b41c2415e5798
Matthew Howard <mhoward14@icloud.com>
2018-07-17 20:02:49 -0600

    One-Way Binding Exercise

---
+++ week-2/one-way-binding/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>{{ name}}</h1> <button (click)="changeName()">Change Name</button>',
+  styles: [
+    'h1 { color: red; }'
+  ]
+})
+export class AppComponent {
+  title = 'app';
+
+  name:string = 'Matthew Howard';
+
+  changeName() {
+    this.name = 'Matthew Howard';
+  }
+}
+
